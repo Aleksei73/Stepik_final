4a8dbe3618138cd580f9db4df4b53db78fa6ecca
damir4iki1 <43089145+damir4iki1@users.noreply.github.com>
2021-02-04 00:47:21 +0300

    Modified base and main pages classes

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,8 @@
-import time
-'''link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+from .pages.main_page import MainPage
 
-
-def test_cart_button(browser):
-    browser.get(link)
-    time.sleep(5)
-    x = 0
-    try:
-        browser.find_element_by_css_selector('button[class*="btn-add-to-basket"]')
-        x = 1
-    except Exception:
-        pass
-    finally:
-        assert x == 1, 'Элемент не найден'
-        '''
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
